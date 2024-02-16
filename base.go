d7a7c4c774ee358f8095afc362d8df8cbefd9226
osami <v.pylayev@mail.ru>
2024-02-16 11:16:40 +0400

    sdf

--- base.go
+++ base.go
@@ -1,22 +1,33 @@
 package main
 
-import "fmt"
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
-func sp(count float64, step float64){
-	  resusd:= math.Pow(count, step)
-	  fmt.Println(resusd)
-	
-}
-func ab(x int, y int){
-	fmt.Println("x befor = ", x,  "y befor = ", y)
-	x = x + 20; y = y + 20
-	fmt.Println("x after = " ,x, "y after = ", y)
-}
+func sp(count float64, step float64) {
+	resusd := math.Pow(count, step)
+	fmt.Println(resusd)
 
+}
+func ab(x int, y int) {
+	fmt.Println("x befor = ", x, "y befor = ", y)
+	x = x + 20
+	y = y + 20
+	fmt.Println("x after = ", x, "y after = ", y)
+}
+func add(x, y int) int {
+	fmt.Println(x, " + ", y, " = ", x+y)
+	return x + y
+}
+var mun int = 10
+var p *int
+var k = &mun
 func main() {
 	sp(10, 5)
 	ab(4, 6)
-	}
-
-
+	add(10, 657)
+	var gul int = add(5,7)
+	fmt.Println(gul)
+	fmt.Println(*k)
+}
