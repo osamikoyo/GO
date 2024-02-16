cc2f4286a54254f8a67df8d70396e22abe5e4320
osamikoyo <151423319+osamikoyo@users.noreply.github.com>
2024-02-16 14:23:56 +0400

    Add files via upload

---
+++ Figure.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+var a int = 5
+var b int = 10
+var c int = 5
+var d int = 10
+var s int = a*b
+var PER int = 2*(a+b)
+
+const PI float32 = 3.141592653589793 //initialization
+
+
+func in() {
+	fmt.Println()
+}
